d5485c26305713f4f6e869ec7f15d9eac3a66cdc
Berislav Lopac <berislav@lopac.net>
2020-05-03 11:26:27 +0100

    cleanup with black

--- lambda.py
+++ lambda.py
@@ -3,14 +3,14 @@ from base64 import b64encode
 
 import boto3
 
-page_template = '<html><body><ul>{}</ul></body></html>'
+page_template = "<html><body><ul>{}</ul></body></html>"
 list_template = '<li><a href="{path}">{name}</a>'
 
-S3_BUCKET = os.environ.get('S3_BUCKET')
+S3_BUCKET = os.environ.get("S3_BUCKET")
 
-FILE_TYPES = {'egg', 'whl', 'gz'}
+FILE_TYPES = {"egg", "whl", "gz"}
 
-s3 = boto3.resource('s3')
+s3 = boto3.resource("s3")
 
 
 def is_allowed_type(path):
@@ -23,14 +23,14 @@ def get_objects():
 
 
 def parse_path(path):
-    package, *filename = path.split('/')
-    return package, ''.join(filename)
+    package, *filename = path.split("/")
+    return package, "".join(filename)
 
 
 def handler(event, context):
     objects = get_objects()
 
-    path = event.get('path', '/')[1:]
+    path = event.get("path", "/")[1:]
     package, filename = parse_path(path)
 
     if package:
@@ -41,9 +41,9 @@ def handler(event, context):
             if path in objects and is_allowed_type(path):
                 summary = objects[path]
                 obj = s3.Object(summary.bucket_name, summary.key)
-                local_path = f'/tmp/{filename}'
+                local_path = f"/tmp/{filename}"
                 obj.download_file(local_path)
-                with open(local_path, 'rb') as file:
+                with open(local_path, "rb") as file:
                     data = file.read()
                 data = b64encode(data)
                 return {
@@ -52,10 +52,10 @@ def handler(event, context):
                         "Content-Disposition": f"attachment; filename={filename}",
                         "Content-Type": "application/zip, application/octet-stream",
                     },
-                    "body": data.decode()
+                    "body": data.decode(),
                 }
             else:
-                raise Exception('Invalid file requested.')
+                raise Exception("Invalid file requested.")
 
         # list package files
         buffer = len(package) + 1
@@ -65,18 +65,22 @@ def handler(event, context):
             print(package, key)
             filename = key[buffer:] if key.startswith(package) else key
             print(filename)
-            if '/' not in filename and is_allowed_type(filename):
+            if "/" not in filename and is_allowed_type(filename):
                 items[filename] = filename
 
     else:
 
         # list packages
-        items = {key: key for key in objects if key.endswith('/') and '/' not in key[:-1]}
+        items = {
+            key: key for key in objects if key.endswith("/") and "/" not in key[:-1]
+        }
 
-    listing = sorted(list_template.format(path=key, name=value) for key, value in items.items())
+    listing = sorted(
+        list_template.format(path=key, name=value) for key, value in items.items()
+    )
 
     return {
         "isBase64Encoded": False,
         "headers": {"Content-Type": "text/html"},
-        "body": page_template.format("\n".join(listing))
+        "body": page_template.format("\n".join(listing)),
     }
